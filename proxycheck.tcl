d32c5d6f7d06d8790e3c642968ee96a8d0ab9b06
udin <66184870+u-din@users.noreply.github.com>
2022-01-24 08:02:09 +0700

    Update proxycheck.tcl

--- proxycheck.tcl
+++ proxycheck.tcl
@@ -1,8 +1,8 @@
 ##################################################################################
 ## Check IP From Proxycheck.io                                                  ##
-## Simple CovidIP TCL By iJoo                                                   ##
+## Simple ProxyCheck TCL By iJoo                                                ##
 ##                                                                              ##
-##                                                                   02/10/2021 ##
+## Penambahan Region (Wilayah) edited by isfan                       02/10/2021 ##
 ##################################################################################
 
 package require http
@@ -11,7 +11,7 @@ http::register https 443 [list ::tls::socket -autoservername true]
 
 ## Siapa yang bisa command? n = owner
 set akses ""
-set perintah "!cp"
+set perintah "!cek"
 
 bind pub $akses $perintah ijoo_ganteng
 
@@ -25,13 +25,14 @@ proc ijoo_ganteng {nick uhost hand chan rest} {
         } else {
                 regexp {"asn": "(.*?)"} [http::data $token] -> ijoo_asn
                 regexp {"provider": "(.*?)"} [http::data $token] -> ijoo_prov
+		regexp {"region": "(.*?)"} [http::data $token] -> ijoo_reg
                 regexp {"continent": "(.*?)"} [http::data $token] -> ijoo_cont
                 regexp {"country": "(.*?)"} [http::data $token] -> ijoo_cont2
                 regexp {"isocode": "(.*?)"} [http::data $token] -> ijoo_code
                 regexp {"proxy": "(.*?)"} [http::data $token] -> ijoo_prox
                 regexp {"type": "(.*?)"} [http::data $token] -> ijoo_type
-                putquick "PRIVMSG $chan :IP:\002 $chkip\002, Provider Oleh:\002 $ijoo_prov\002, ASN:\002 $ijoo_asn\002, Lokasi:\002 $ijoo_cont, $ijoo_cont2 ($ijoo_code)\002, Proxy Status:\002 $ijoo_prox ($ijoo_type)\002"
+                putquick "PRIVMSG $chan :IP:\002 $chkip\002, Provider Oleh:\002 $ijoo_prov\002, ASN:\002 $ijoo_asn\002, Lokasi:\002 $ijoo_reg, $ijoo_cont2, $ijoo_cont ($ijoo_code)\002, Proxy Status:\002 $ijoo_prox ($ijoo_type)\002"
         }
 }
 
-putlog "proxycheck is Loaded!"
+putlog "CovidIP is Loaded!"
