c00393cb727826dbfdf640dad0e67e9f1c16cdbe
udin <66184870+u-din@users.noreply.github.com>
2022-08-15 09:15:45 +0700

    Update uno.tcl

--- uno/uno.tcl
+++ uno/uno.tcl
@@ -23,11 +23,11 @@
 # default settings (these are overridden by uno.cfg)
 set UnoAds		0
 set UnoDebug 		0
-set UnoChan 		"#squad"
+set UnoChan 		"#uno"
 set UnoRobot 		$botnick
 set UnoPointsName 	"Points"
 set UnoStopAfter 	3
-set UnoJoinAnyTime	1
+set UnoJoinAnyTime	0
 set UnoUseDCC		0
 set UnoBonus		0
 set UnoWildDrawTwos	0
@@ -38,7 +38,7 @@ set UnoOpFlags		"-"
 set UnoNTC		"NOTICE"
 set UnoCFGFile		"scripts/uno.cfg"
 set UnoScoreFile 	"UnoScores"
-set UnoVersion 		"Squad UNO"
+set UnoVersion 		"UNO"
 
 # command binds
 bind pub - .uno UnoInit
@@ -333,7 +333,7 @@ proc UnoNext {} {
 
  set MasterDeck [list B0 B1 B1 B2 B2 B3 B3 B4 B4 B5 B5 B6 B6 B7 B7 B8 B8 B9 B9 BR BR BS BS BD BD R0 R1 R1 R2 R2 R3 R3 R4 R4 R5 R5 R6 R6 R7 R7 R8 R8 R9 R9 RR RR RS RS RD RD Y0 Y1 Y1 Y2 Y2 Y3 Y3 Y4 Y4 Y5 Y5 Y6 Y6 Y7 Y7 Y8 Y8 Y9 Y9 YR YR YS YS YD YD G0 G1 G1 G2 G2 G3 G3 G4 G4 G5 G5 G6 G6 G7 G7 G8 G8 G9 G9 GR GR GS GS GD GD W W W W WD WD WD WD]
 
- unochanmsg "$UnoVersion 4@ Chating.id"
+ unochanmsg "$UnoVersion 4@ withyou.id"
 
  set done 0
  while {!$done} {
